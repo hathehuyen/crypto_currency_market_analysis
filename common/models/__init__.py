7a05cd75baea8e35e3962b067c1bd66bb16b60ee
Huyen Ha <huyenht@vp9.vn>
2017-08-28 22:39:39 +0700

    Add requirements.txt

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -5,7 +5,7 @@ connect('ccma', host='localhost')
 
 
 class Coin(Document):
-    id = StringField(max_length=30)
+    coin_id = StringField(max_length=30)
     name = StringField(max_length=200)
     symbol = StringField(max_length=30)
     rank = IntField()

680a66f2573bea46df986eee329be37c5c3d3a0c
Huyen Ha <huyenht@vp9.vn>
2017-08-28 23:05:16 +0700

    Add requirements.txt

--- collector.py
+++ collector.py
@@ -22,9 +22,9 @@ for coin in coin_list:
         new_coin.market_cap_usd = float(coin['market_cap_usd'])
         new_coin.available_supply = float(coin['available_supply'])
         new_coin.total_supply = float(coin['total_supply'])
-        new_coin.percent_change_1h = float(coin['percent_change_1h'])
-        new_coin.percent_change_24h = float(coin['percent_change_24h'])
-        new_coin.percent_change_7d = float(coin['percent_change_7d'])
+        new_coin.percent_change_1h = float(coin['percent_change_1h'] if coin['percent_change_1h'] else 0)
+        new_coin.percent_change_24h = float(coin['percent_change_24h'] if coin['percent_change_24h'] else 0)
+        new_coin.percent_change_7d = float(coin['percent_change_7d'] if coin['percent_change_7d'] else 0)
         new_coin.last_updated = int(coin['last_updated'])
         new_coin.save()
     else:

aa29b399051b716409fae3bd5a66c857d18869d1
Huyen Ha <huyenht@vp9.vn>
2017-08-29 22:53:05 +0700

    Re-models

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -4,6 +4,12 @@ from mongoengine import *
 connect('ccma', host='localhost')
 
 
+class Coin(Document):
+    coin_id = StringField(max_length=100)
+    name = StringField(max_length=200)
+    symbol = StringField(max_length=30)
+
+
 class CoinTicker(Document):
     coin = ReferenceField('Coin')
     coin_id = StringField(max_length=100)
@@ -21,7 +27,3 @@ class CoinTicker(Document):
     percent_change_7d = FloatField()
     last_updated = IntField()
 
-class Coin(Document):
-    coin_id = StringField(max_length=100)
-    name = StringField(max_length=200)
-    symbol = StringField(max_length=30)
